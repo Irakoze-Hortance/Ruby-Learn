733631621d5794c6def7697251e42ec5faacb980
Irakoze Hortance <54577367+Irakoze-Hortance@users.noreply.github.com>
2021-06-14 08:29:50 -0700

    added diplay details function

--- classes.rb
+++ classes.rb
@@ -11,10 +11,17 @@ class Student
     def to_s
         print (@id + ' '+@fname+' '+@lname+' '+@gender)
     end
+
+    def display_details
+        puts "Student name is '#{@fname} #{@lname}'"
+        puts "Student gender is '#{@gender}'"
+    end
 end
 
 stud1=Student.new("1","Denyse","Mutoni","Female")
 # print stud1.inspect
 print stud1.to_s
+print "\n"
+stud1.display_details
 
 
